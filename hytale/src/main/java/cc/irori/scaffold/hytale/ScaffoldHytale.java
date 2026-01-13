04cd53ec4355989ad39d5725b1c8fdcad1d8dc8f
KabanFriends <yuukun2004@protonmail.ch>
2026-01-14 03:45:45 +0900

    feat: ✨ add sendChatMessage implementation

--- hytale/src/main/java/cc/irori/scaffold/hytale/ScaffoldHytale.java
+++ hytale/src/main/java/cc/irori/scaffold/hytale/ScaffoldHytale.java
@@ -3,12 +3,18 @@ package cc.irori.scaffold.hytale;
 import cc.irori.scaffold.discord.Scaffold;
 import cc.irori.scaffold.discord.config.ConfigProvider;
 import cc.irori.scaffold.discord.util.Logs;
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.event.events.player.PlayerChatEvent;
 import com.hypixel.hytale.server.core.event.events.player.PlayerConnectEvent;
 import com.hypixel.hytale.server.core.event.events.player.PlayerDisconnectEvent;
+import com.hypixel.hytale.server.core.universe.Universe;
 import org.slf4j.Logger;
 
+import java.awt.*;
+
 public class ScaffoldHytale extends Scaffold {
+
+    private static final Color DISCORD_PREFIX_COLOR = new Color(0x7070FF);
 
     private static final Logger LOGGER = Logs.logger();
 
@@ -36,6 +42,9 @@ public class ScaffoldHytale extends Scaffold {
 
     @Override
     public void sendChatMessage(String sender, String message) {
-        LOGGER.warn("Not implemented!");
+        Universe.get().sendMessage(Message.join(
+                Message.raw("[D] @" + sender + ": ").color(DISCORD_PREFIX_COLOR),
+                Message.raw(message)
+        ));
     }
 }
