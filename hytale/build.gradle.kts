230a4f651627033d1e211f16de9302d9ca0aca57
KabanFriends <yuukun2004@protonmail.ch>
2026-01-23 15:19:19 +0900

    build: 📦 use official HytaleServer maven artifact

--- hytale/build.gradle.kts
+++ hytale/build.gradle.kts
@@ -3,15 +3,19 @@ plugins {
     alias(libs.plugins.gradle.shadow)
 }
 
+repositories {
+    maven("https://maven.hytale.com/release")
+    maven("https://maven.hytale.com/pre-release")
+}
+
 dependencies {
     implementation(project(":discord"))
-
-    compileOnly(files("libs/HytaleServer.jar"))
 
     implementation(libs.guava)
     implementation(libs.lang3)
     // TODO: don't include SLF4j, HytaleServer does not log it correctly
     implementation(libs.slf4j.api)
 
+    compileOnly(libs.hytale)
     compileOnly(libs.shodo)
 }
